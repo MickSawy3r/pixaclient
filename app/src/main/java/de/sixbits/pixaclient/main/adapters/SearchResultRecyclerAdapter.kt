829723e6de5e4e6bf200e5d5969bffb8cc821a71
mohammad <mohammad.al.kalaleeb@gmail.com>
2021-04-23 20:22:49 +0300

    Preloading with Glide Works!

--- app/src/main/java/de/sixbits/pixaclient/main/adapters/SearchResultRecyclerAdapter.kt
+++ app/src/main/java/de/sixbits/pixaclient/main/adapters/SearchResultRecyclerAdapter.kt
@@ -1,15 +1,20 @@
 package de.sixbits.pixaclient.main.adapters
 
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
+import com.bumptech.glide.ListPreloader
+import com.bumptech.glide.RequestBuilder
 import de.sixbits.pixaclient.databinding.RowImagesListBinding
 import de.sixbits.pixaclient.network.model.ImageListItemModel
 
-class SearchResultRecyclerAdapter constructor(private val searchResult: List<ImageListItemModel>) :
-    RecyclerView.Adapter<SearchResultRecyclerAdapter.SearchResultRecyclerViewHolder>() {
+class SearchResultRecyclerAdapter constructor(
+    private val searchResult: List<ImageListItemModel>,
+    private val requestBuilder: RequestBuilder<Drawable>
+) :
+    RecyclerView.Adapter<SearchResultRecyclerAdapter.SearchResultRecyclerViewHolder>(),
+    ListPreloader.PreloadModelProvider<ImageListItemModel> {
 
     class SearchResultRecyclerViewHolder(val binding: RowImagesListBinding) :
         RecyclerView.ViewHolder(binding.root)
@@ -30,7 +35,20 @@ class SearchResultRecyclerAdapter constructor(private val searchResult: List<Ima
     override fun onBindViewHolder(holder: SearchResultRecyclerViewHolder, position: Int) {
         holder.binding.tvImageItemUsername.text = searchResult[position].username
         holder.binding.tvImageItemTags.text = searchResult[position].tags
+        requestBuilder.load(searchResult[position].thumbnail)
+            .into(holder.binding.ivImageItemThumbnail)
     }
 
     override fun getItemCount(): Int = searchResult.size
+
+    override fun getPreloadItems(position: Int): MutableList<ImageListItemModel> {
+        return if (searchResult.isNotEmpty())
+            mutableListOf(searchResult[position])
+        else
+            mutableListOf()
+    }
+
+    override fun getPreloadRequestBuilder(item: ImageListItemModel): RequestBuilder<*> {
+        return requestBuilder.load(item)
+    }
 }
--- app/src/main/java/de/sixbits/pixaclient/main/ui/MainActivity.kt
+++ app/src/main/java/de/sixbits/pixaclient/main/ui/MainActivity.kt
@@ -1,22 +1,23 @@
 package de.sixbits.pixaclient.main.ui
 
-import android.opengl.Visibility
+import android.annotation.SuppressLint
+import android.graphics.drawable.Drawable
 import android.os.Bundle
-import android.util.Log
 import android.view.View.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestBuilder
+import com.bumptech.glide.integration.recyclerview.RecyclerViewPreloader
+import com.bumptech.glide.util.ViewPreloadSizeProvider
 import de.sixbits.pixaclient.MyApplication
-import de.sixbits.pixaclient.R
-import de.sixbits.pixaclient.databinding.ActivityDetailsBinding
 import de.sixbits.pixaclient.databinding.ActivityMainBinding
 import de.sixbits.pixaclient.main.MainComponent
 import de.sixbits.pixaclient.main.adapters.SearchResultRecyclerAdapter
 import de.sixbits.pixaclient.main.view_model.MainViewModel
+import de.sixbits.pixaclient.network.model.ImageListItemModel
 import javax.inject.Inject
-
-private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     @Inject
@@ -24,10 +25,9 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var mainComponent: MainComponent
     private lateinit var mainViewModel: MainViewModel
-
     private lateinit var binding: ActivityMainBinding
-
     private lateinit var searchRecyclerAdapter: SearchResultRecyclerAdapter
+    private lateinit var searchRecyclerRequestBuilder: RequestBuilder<Drawable>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         // Inject
@@ -52,7 +52,9 @@ class MainActivity : AppCompatActivity() {
             .get(MainViewModel::class.java)
 
         mainViewModel.imageListLiveData.observe(this, {
-            searchRecyclerAdapter = SearchResultRecyclerAdapter(it)
+            searchRecyclerAdapter =
+                SearchResultRecyclerAdapter(it, requestBuilder = searchRecyclerRequestBuilder)
+            binding.rvSearchResult.adapter = searchRecyclerAdapter
         })
         mainViewModel.loadingLiveData.observe(this, {
             if (it) {
@@ -63,12 +65,33 @@ class MainActivity : AppCompatActivity() {
                 binding.rvSearchResult.visibility = VISIBLE
             }
         })
+
+        mainViewModel.searchFor("Fruits")
     }
 
+    @SuppressLint("SetTextI18n")
     private fun initViews() {
         binding.rvSearchResult.layoutManager = LinearLayoutManager(this)
         binding.tilSearchBar.setEndIconOnClickListener {
             mainViewModel.searchFor(binding.etSearchBar.text.toString())
         }
+        binding.etSearchBar.setText("Fruits")
+
+        searchRecyclerRequestBuilder = Glide.with(this)
+            .asDrawable()
+        val preloadSizeProvider =
+            ViewPreloadSizeProvider<ImageListItemModel>()
+
+        searchRecyclerAdapter = SearchResultRecyclerAdapter(listOf(), searchRecyclerRequestBuilder)
+
+        val preLoader =
+            RecyclerViewPreloader(
+                Glide.with(this),
+                searchRecyclerAdapter,
+                preloadSizeProvider,
+                4
+            )
+
+        binding.rvSearchResult.addOnScrollListener(preLoader)
     }
 }
