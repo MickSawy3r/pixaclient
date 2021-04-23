d766aa2bd0759bd7c259df5b2c06e12b63dd9479
mohammad <mohammad.al.kalaleeb@gmail.com>
2021-04-23 10:17:22 +0300

    main repository is created

--- app/src/main/java/de/sixbits/pixaclient/main/repository/MainRepository.kt
+++ app/src/main/java/de/sixbits/pixaclient/main/repository/MainRepository.kt
@@ -1,8 +1,15 @@
 package de.sixbits.pixaclient.main.repository
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import de.sixbits.pixaclient.database.dao.CacheDao
 import de.sixbits.pixaclient.network.manager.PixabayManager
+import de.sixbits.pixaclient.network.model.ImageListItemModel
+import io.reactivex.Observable
 import javax.inject.Inject
 
-class MainRepository @Inject constructor(pixabayManager: PixabayManager, cacheDao: CacheDao) {
+class MainRepository @Inject constructor(private val pixabayManager: PixabayManager) {
+    fun searchFor(query: String) : Observable<List<ImageListItemModel>> {
+        return this.pixabayManager.getSearchResult(query)
+    }
 }
