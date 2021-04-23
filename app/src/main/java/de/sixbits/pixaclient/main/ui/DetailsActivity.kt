74b77f5f9373df325e0b85e2f1cdaa651bc469a7
mohammad <mohammad.al.kalaleeb@gmail.com>
2021-04-23 11:00:35 +0300

    Details view model is live

---
+++ app/src/main/java/de/sixbits/pixaclient/main/ui/DetailsActivity.kt
@@ -0,0 +1,12 @@
+package de.sixbits.pixaclient.main.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import de.sixbits.pixaclient.R
+
+class DetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
--- app/src/main/java/de/sixbits/pixaclient/main/view_model/DetailsViewModel.kt
+++ app/src/main/java/de/sixbits/pixaclient/main/view_model/DetailsViewModel.kt
@@ -1,8 +1,30 @@
 package de.sixbits.pixaclient.main.view_model
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import de.sixbits.pixaclient.main.repository.DetailsRepository
+import de.sixbits.pixaclient.network.model.ImageDetailsModel
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
-class DetailsViewModel : ViewModel() {
+class DetailsViewModel @Inject constructor(private val detailsRepository: DetailsRepository) :
+    ViewModel() {
 
+    val detailsLiveData = MutableLiveData<ImageDetailsModel>()
+
+    fun getImageDetails(imageId: Int) {
+        val disposable = CompositeDisposable()
+
+        val repo = detailsRepository.getImageDetails(imageId)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe {
+                detailsLiveData.postValue(it)
+            }
+
+        // a good developer always cleans up!
+        disposable.add(repo)
+    }
 }
