8ccf74fac19b00da0f8590427e13738e06b33d2c
mohammad <mohammad.al.kalaleeb@gmail.com>
2021-04-24 13:19:30 +0300

    Rules work!

--- app/src/androidTest/java/de/sixbits/pixaclient/unit/main/view_model/TestMainViewModel.kt
+++ app/src/androidTest/java/de/sixbits/pixaclient/unit/main/view_model/TestMainViewModel.kt
@@ -1,4 +1,25 @@
 package de.sixbits.pixaclient.unit.main.view_model
 
+import androidx.test.core.app.ActivityScenario
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import de.sixbits.pixaclient.main.ui.MainActivity
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4ClassRunner::class)
 class TestMainViewModel {
+
+
+    @Before
+    fun setup() {
+    }
+
+    @After
+    fun teardown() {
+
+    }
 }
