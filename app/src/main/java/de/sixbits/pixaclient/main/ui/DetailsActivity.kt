a6669364b2e176bb588d7a3e60a736eb0bb8d75e
mohammad <mohammad.al.kalaleeb@gmail.com>
2021-04-26 12:18:13 +0300

    cleaning up

--- app/src/main/java/de/sixbits/pixaclient/main/ui/DetailsActivity.kt
+++ app/src/main/java/de/sixbits/pixaclient/main/ui/DetailsActivity.kt
@@ -1,36 +1,25 @@
 package de.sixbits.pixaclient.main.ui
 
-import android.animation.Animator
-import android.animation.AnimatorListenerAdapter
-import android.animation.AnimatorSet
-import android.animation.ObjectAnimator
-import android.graphics.Point
-import android.graphics.Rect
-import android.graphics.RectF
 import android.os.Bundle
-import android.view.View
-import android.view.animation.DecelerateInterpolator
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
 import de.sixbits.pixaclient.MyApplication
-import de.sixbits.pixaclient.R
 import de.sixbits.pixaclient.databinding.ActivityDetailsBinding
 import de.sixbits.pixaclient.main.MainComponent
 import de.sixbits.pixaclient.main.keys.IntentKeys
 import de.sixbits.pixaclient.main.view_model.DetailsViewModel
 import javax.inject.Inject
 
-private const val TAG = "DetailsActivity"
 
 class DetailsActivity : AppCompatActivity() {
     // Injected members
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
-    lateinit var mainComponent: MainComponent
 
-    lateinit var detailsViewModel: DetailsViewModel
+    private lateinit var mainComponent: MainComponent
+
+    private lateinit var detailsViewModel: DetailsViewModel
     private lateinit var binding: ActivityDetailsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
